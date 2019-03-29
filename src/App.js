e9f844da0ca8236a1404b2d10b20c8ffc69aa91b
Puyan Wei <puyan.wei@mountainwarehouse.com>
2019-03-29 11:55:41 +0000

    Formalised button structure

--- src/App.js
+++ src/App.js
@@ -16,21 +16,29 @@ const App = () => {
                 <div className="calculator">
                     <input className="window" type="text" value={currentNumber} readOnly />
                     <div className="row">
-                        <Button handleClick={handleClick} label={1} />
-                        <Button handleClick={handleClick} label={2} />
-                        <Button handleClick={handleClick} label={3} />
+                        <Button handleClick={handleClick} label="1" />
+                        <Button handleClick={handleClick} label="2" />
+                        <Button handleClick={handleClick} label="3" />
+                        <Button handleClick={handleClick} label={'+'} />
                     </div>
                     <div className="row">
-                        <Button handleClick={handleClick} label={4} />
-                        <Button handleClick={handleClick} label={5} />
-                        <Button handleClick={handleClick} label={6} />
+                        <Button handleClick={handleClick} label="4" />
+                        <Button handleClick={handleClick} label="5" />
+                        <Button handleClick={handleClick} label="6" />
+                        <Button handleClick={handleClick} label="-" />
                     </div>
                     <div className="row">
-                        <Button handleClick={handleClick} label={7} />
-                        <Button handleClick={handleClick} label={8} />
-                        <Button handleClick={handleClick} label={9} />
+                        <Button handleClick={handleClick} label="7" />
+                        <Button handleClick={handleClick} label="8" />
+                        <Button handleClick={handleClick} label="9" />
+                        <Button handleClick={handleClick} label="x" />
                     </div>
-                    <Button handleClick={handleClick} label={0} />
+                    <div className="row">
+                        <Button handleClick={handleClick} label="↤" />
+                        <Button handleClick={handleClick} label="0" />
+                        <Button handleClick={handleClick} label="=" />
+                        <Button handleClick={handleClick} label="÷" />
+                    </div>
                 </div>
             </div>
         </div>
